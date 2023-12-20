4728140dd94db5537f22f9526a84592bc3a05840
voganhaov <152450101+voganhaov@users.noreply.github.com>
2023-12-20 15:54:43 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,54 @@
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+import random
+
+
+def main():
+    for item in data:
+    main()
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":    const data = [];
+    const data = generateRandomData();
+function generateRandomData() {
+    return data;
+
+}
+function main() {
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    }
+
+        console.log("Random Number: " + item);
+}
+    });
+main();    public static void main(String[] args) {
+        int[] data = new int[10];
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+
+    }
+}
+        }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+
+    main()
+if __name__ == "__main__":
+import random
+    for item in data:
+
+def main():
